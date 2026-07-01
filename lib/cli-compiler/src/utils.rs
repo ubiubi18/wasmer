ba4035caf5329f495a65ab1e61a238b742c99b3c
ubiubi18 <ubiubi18@users.noreply.github.com>
2026-07-01 14:24:14 +0200

    Remove stale Wasmer test lockfile and direct atty use

--- lib/cli-compiler/src/utils.rs
+++ lib/cli-compiler/src/utils.rs
@@ -1,6 +1,7 @@
 //! Utility functions for the WebAssembly module
 use anyhow::{bail, Result};
 use std::env;
+use std::io::{self, IsTerminal};
 use std::path::PathBuf;
 
 /// Whether or not Wasmer should print with color
@@ -8,7 +9,7 @@ pub fn wasmer_should_print_color() -> bool {
     env::var("WASMER_COLOR")
         .ok()
         .and_then(|inner| inner.parse::<bool>().ok())
-        .unwrap_or_else(|| atty::is(atty::Stream::Stdout))
+        .unwrap_or_else(|| io::stdout().is_terminal())
 }
 
 fn retrieve_alias_pathbuf(alias: &str, real_dir: &str) -> Result<(String, PathBuf)> {
